9f9f7ec499a8615c21466efc5326eb3ece794bc1
anabrandt <anactavaresbrand@gmail.com>
2023-09-10 18:31:13 -0300

    rm552283

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom"
 import logoPorto from "./img/logoPorto.png"
+import profile from "./img/iconeprofile.png"
 
 export default function Menu(){
 
     return(
         <nav>
-            <Link to='/' >Home</Link>
-            <div className="logo__porto">
+            <div>
             <img src={logoPorto} alt="logo da porto seguro" />
             </div>
+
             <span>  </span>
+
+            <div>
+            <img src={profile} alt="icone de usuario" />   
             <Link to='/Entrar'>Entrar</Link>
+            </div>
         </nav>
     )
 }
